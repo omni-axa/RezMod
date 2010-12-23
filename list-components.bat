b4141f02bf9405bc9510f01c39cd47072a165e3d
omni-axa <kgoon007@gmail.com>
2010-12-23 09:44:49 +0530

    component listing stuff

---
+++ list-components.bat
@@ -0,0 +1,12 @@
+:: Replace SETUP-#######.TP2 with the file name you are looking for.
+:: Note: With some mods it may also be #######/SETUP-#######.TP2
+:: Replace 0 with the number for your language.
+:: Run the batch file in the BGII directory and see the list-components.txt.
+
+:: Ersetzt ####.### durch den Dateinamen, nach dem Ihr sucht.
+:: Beachtet: Bei einigen Mods kann das auch  #######/SETUP-#######.TP2 sein
+:: Ersetzt 0 durch die Ziffer für Eure Sprache.
+:: Führt die Batch-Datei im BGII Verzeichnis aus und seht in die list-components.txt.
+
+
+weidu.exe --nogame --list-components RezMod\Setup-RezMod.tp2 0  >list-components.txt
